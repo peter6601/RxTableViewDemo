63b84686099e6a70142d5c77973af09938785ff2
DinDin <din.din@isunCloud.com>
2018-11-06 18:30:57 +0800

    add register bind

--- RxTableViewBindingDemo/RegisterViewController.swift
+++ RxTableViewBindingDemo/RegisterViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var btnRegister: UIButton!
     @IBOutlet weak var mainTableView: UITableView! {
         didSet {
@@ -18,37 +20,64 @@ class RegisterViewController: UIViewController {
         }
     }
     var viewModel: RegisterViewModel!
+    var disposeBag: DisposeBag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.bindViewModel(list: nil)  
+        self.btnRegister.addTarget(self, action: #selector(tap), for: .touchUpInside)
+//        self.viewModel.bindtapButton(self.btnRegister.rx.tap.asDriver())
 
-        // Do any additional setup after loading the view.
     }
+    
+    @objc func tap() {
+        
+    }
+    
     
     func bindViewModel(list: [RegisterViewModel.RegisterType]?) {
         self.viewModel = RegisterViewModel(list)
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
 extension RegisterViewController: UITableViewDataSource {
-   
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.list.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "InputTextFieldTableViewCell", for: indexPath) as?  InputTextFieldTableViewCell
-        return cell!
+        let cellType =  viewModel.list[indexPath.row]
+        switch cellType {
+            
+        case .name(let title, let input, let info):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "InputTextFieldTableViewCell", for: indexPath) as?  InputTextFieldTableViewCell
+            cell?.llTitle.text = title
+            cell?.llInfo.text = info
+            cell?.tfInput.rx.text.asDriver().drive(input).disposed(by: disposeBag)
+            let a = cell?.llInfo.rx.isHidden
+            
+            return cell!
+
+        case .password(let title, let input, let info):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "InputTextFieldTableViewCell", for: indexPath) as?  InputTextFieldTableViewCell
+            cell?.llTitle.text = title
+            cell?.llInfo.text = info
+            cell?.tfInput.rx.text.asDriver().drive(input).disposed(by: disposeBag)
+            return cell!
+
+        }
     }
     
     
--- RxTableViewBindingDemo/RegisterViewModel.swift
+++ RxTableViewBindingDemo/RegisterViewModel.swift
@@ -7,21 +7,41 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class RegisterViewModel {
     
     var list = [RegisterType]()
-    
+    var name: BehaviorRelay<String?> = BehaviorRelay.init(value: nil)
+    var password: BehaviorRelay<String?> = BehaviorRelay.init(value: nil)
+    var nameInfoisHidden: Observable<Bool> = Observable.just(true)
+    var passwordInfoisHidden: Observable<Bool> = Observable.just(true)
+
     enum RegisterType {
-        case name(title: String, input: String?, info: String?)
-        case password(title: String, input: String?, info: String?)
+        case name(title: String, input: BehaviorRelay<String?>, info: String?)
+        case password(title: String, input: BehaviorRelay<String?>, info: String?)
     }
     init(_ list:  [RegisterType]?) {
         guard let _list = list else {
-            self.list = [RegisterType.name(title: "Name", input: nil, info: nil), RegisterType.password(title: "Password", input: nil, info: nil)]
+            self.list = [RegisterType.name(title: "Name", input: name, info: nil), RegisterType.password(title: "Password", input: password, info: nil)]
             return
         }
         self.list = _list
+    }
+    
+    func bindtapButton(_ didTapButton: Driver<Void>) {
+        didTapButton.drive(onNext: { (_) in
+            
+        })
+    }
+    
+    func checkbind(_ name: BehaviorRelay<String?>) {
+        name.subscribe(onNext: { (info) in
+            if let _info = info, _info.count <= 5 {
+             self.nameInfoisHidden = Observable.just(false)
+            }
+        })
     }
     
     
